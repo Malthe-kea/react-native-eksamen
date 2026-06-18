fd646e916dc2ae917317ea9768bb9c1d686e9d86
malthe-kea <mati0002@stud.kea.dk>
2026-06-18 11:51:17 +0200

    Refactor og formattering

--- StreetArtRater/App.js
+++ StreetArtRater/App.js
@@ -7,7 +7,6 @@ import { auth } from "./firebase";
 
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
-
 import HomeScreen from "./screens/HomeScreen";
 import AddArtScreen from "./screens/AddArtScreen";
 import ListScreen from "./screens/ListScreen";
@@ -31,86 +30,53 @@ export default function App() {
         return unsubscribe;
     }, []);
 
-    if (loading) {
-        return null;
-    }
+    if (loading) return null;
 
     return (
         <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false,
-                }}
-            >
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
                 {user ? (
                     <>
                         <Stack.Screen
                             name="Home"
                             component={HomeScreen}
-                            options={{
-                                title: "Street Art",
-                            }}
+                            options={{ title: "Street Art" }}
                         />
-
                         <Stack.Screen
                             name="AddArt"
                             component={AddArtScreen}
-                            options={{
-                                title: "Add art",
-                            }}
+                            options={{ title: "Add art" }}
                         />
-
                         <Stack.Screen
                             name="List"
                             component={ListScreen}
-                            options={{
-                                title: "List of street art",
-                            }}
+                            options={{ title: "List of street art" }}
                         />
-
                         <Stack.Screen
                             name="Detail"
                             component={DetailScreen}
-                            options={{
-                                title: "Details",
-                            }}
+                            options={{ title: "Details" }}
                         />
-
                         <Stack.Screen
                             name="MyStreetArt"
                             component={MyStreetArtScreen}
-                            options={{
-                                title: "My street art",
-                            }}
+                            options={{ title: "My street art" }}
                         />
-
                         <Stack.Screen
                             name="Map"
                             component={MapScreen}
-                            options={{
-                                title: "Street Art Map",
-                            }}
+                            options={{ title: "Street Art Map" }}
                         />
-
                         <Stack.Screen
                             name="TopRated"
                             component={TopRatedScreen}
-                            options={{
-                                title: "Highest rated",
-                            }}
+                            options={{ title: "Highest rated" }}
                         />
                     </>
                 ) : (
                     <>
-                        <Stack.Screen
-                            name="Login"
-                            component={LoginScreen}
-                        />
-
-                        <Stack.Screen
-                            name="Signup"
-                            component={SignupScreen}
-                        />
+                        <Stack.Screen name="Login" component={LoginScreen} />
+                        <Stack.Screen name="Signup" component={SignupScreen} />
                     </>
                 )}
             </Stack.Navigator>
